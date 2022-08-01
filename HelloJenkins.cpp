a1df218f17d8ccb2102328d19570ca1b3a62f858
maggu95 <magnus.roen.johnsen@live.no>
2022-08-01 13:24:01 +0200

    Update HelloJenkins.cpp

--- HelloJenkins.cpp
+++ HelloJenkins.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello Jenkins!\n";
+    std::cout << "Hello Jenkins! Dette er en ny commit\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
